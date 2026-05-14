87b2d4175da479c9665bb8d50db1eb55c0030a29
Nishan0920 <nishanbista336@gmail.com>
2026-05-14 12:00:07 +0545

    up

--- db.js
+++ db.js
@@ -5,10 +5,11 @@ dotenv.config();
 
 const ConnectDB = async ()=>{
   try {
-    const MongoDB = await mongoose.connect(process.env.MONGODB_URL)
+    const MongoDB = await mongoose.connect(process.env.MONGODB_URI)
     console.log("Database Connected")
   } catch (error) {
     console.log("Can't Connect the database..")
+    console.log(error)
   }
 }
 export default ConnectDB
