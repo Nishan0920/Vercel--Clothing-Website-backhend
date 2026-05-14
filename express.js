893d88c71cec8557a5ed9861fd22e29a54b283af
Nishan0920 <nishanbista336@gmail.com>
2026-05-14 07:29:33 +0545

    update backhend

--- express.js
+++ express.js
@@ -3,6 +3,7 @@ import ConnectDB from './db.js'
 import Entry from './Routes/User.js'
 import data from './Routes/Data.js'
 import cors from 'cors'
+import serverless from "serverless-http";
 const app = express()
 
 ConnectDB()
@@ -17,4 +18,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-module.exports = app
+export default serverless(app)
